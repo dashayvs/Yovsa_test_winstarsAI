986431ce4eab2a048ab39543c72d56349fe943ce
dashayvs <dashayovsa@gmail.com>
2025-02-15 14:06:54 +0200

    added paths

---
+++ task_1_ImgСls/src/paths.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent.parent
+
+MODELS_DIR = ROOT_DIR / 'models'
+
+RF_CLS_PATH = MODELS_DIR / "rf_cls.joblib"
+FNN_CLS_PATH = MODELS_DIR / "fnn_cls.pth"
+CNN_CLS_PATH = MODELS_DIR / "cnn_cls.pth"
