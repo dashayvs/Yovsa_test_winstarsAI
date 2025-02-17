2b5f71cb2c37695028cc9c714ba1ebb070f8aa21
dashayvs <dashayovsa@gmail.com>
2025-02-17 15:30:14 +0200

    added ner part

---
+++ task_2_NER_ImgCls/src/ner/infer_ner.py
@@ -0,0 +1,20 @@
+import spacy
+import argparse
+from paths import NER_MODEL_PATH
+
+
+def infer_ner(text):
+    nlp = spacy.load(NER_MODEL_PATH)
+
+    doc = nlp(text)
+
+    return [ent.text for ent in doc.ents]
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Perform inference with a trained NER model.")
+    parser.add_argument("--text", type=str, required=True, help="Text to perform NER on")
+    args = parser.parse_args()
+
+    entities = infer_ner(args.text)
+    print(entities)
---
+++ task_2_NER_ImgCls/src/ner/train_ner.py
@@ -0,0 +1,65 @@
+import json
+import spacy
+import random
+from spacy.training import Example
+from spacy.util import minibatch
+import argparse
+from paths import NER_MODEL_PATH, TRAIN_NER
+
+
+def train_ner(train_data, output_dir, iterations=50):
+    nlp = spacy.blank("en")
+    if "ner" not in nlp.pipe_names:
+        ner = nlp.add_pipe("ner", last=True)
+    else:
+        ner = nlp.get_pipe("ner")
+
+    for _, annotations in train_data:
+        for ent in annotations["entities"]:
+            ner.add_label(ent[2])
+
+    examples = []
+    for text, annotations in train_data:
+        doc = nlp.make_doc(text)
+        example = Example.from_dict(doc, annotations)
+        examples.append(example)
+
+    best_loss = float("inf")
+    no_improvement_count = 0
+
+    optimizer = nlp.begin_training()
+
+    for i in range(iterations):
+        random.shuffle(examples)
+        losses = {}
+
+        for batch in minibatch(examples, size=4):
+            nlp.update(batch, drop=0.5, losses=losses)
+
+        loss_value = losses.get("ner", 0)
+        print(f"Iteration {i + 1}, Loss: {loss_value}")
+
+        if best_loss - loss_value > 0.001:
+            best_loss = loss_value
+            no_improvement_count = 0
+        else:
+            no_improvement_count += 1
+
+        if no_improvement_count >= 7:
+            print(f"Early stopping triggered after {i + 1} iterations.")
+            break
+
+
+    nlp.to_disk(NER_MODEL_PATH)
+    print(f"Model saved to {output_dir}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Train an NER model.")
+    parser.add_argument("--iterations", type=int, default=50, help="Number of training iterations")
+    args = parser.parse_args()
+
+    with open(TRAIN_NER, "r") as file:
+        train_data = json.load(file)
+
+    train_ner(train_data, args.output, args.iterations)
